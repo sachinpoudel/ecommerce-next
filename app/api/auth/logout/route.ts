304fc18d5251fcc4cce48a60ca59eb23341d9f41
sachinpoudel <poudelsachin009@gmail.com>
2025-09-24 06:54:06 +0545

    alll auth route

--- app/api/auth/logout/route.ts
+++
@@ -1,3 +0,0 @@
-export const POST = async (request: Request) => {
- return Response.redirect("/api/auth/signout?callbackUrl=/", 302);
-}
--- lib/prisma-client.ts
+++ lib/prisma-client.ts
@@ -1,3 +1,12 @@
-import { PrismaClient } from '@prisma/client'
-export const prisma = new PrismaClient()
- 
+// lib/prisma-client.ts
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
